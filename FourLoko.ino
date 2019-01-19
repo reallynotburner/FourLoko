3b9e62152735f8acdb9d817bde70ba1c6e367876
reallynotburner <reallynotburner@gmail.com>
2019-01-18 22:51:26 -0800

    removed the unused array code from .ino file

--- FourLoko.ino
+++ FourLoko.ino
@@ -2,25 +2,9 @@
 #include "FourLoko.h"
 #include "states.h"
 
-const int arrayHistoryLength = 20;
-int arrayHistory[arrayHistoryLength];
-int arrayHistory2[arrayHistoryLength];
-int loopCount = 0;
-int detectTotal = 0;
-int detectTotal2 = 0;
-
-int currentRightSpeed = 0;
-int currentLeftSpeed = 0;
-int targetRightSpeed = 0;
-int targetLeftSpeed = 0;
-int maximumSlew = 1;
-
 void setup() {
   Serial.begin(115200);
   initPins();
-  for (int i = 0; i < arrayHistoryLength; i++) { // initialize the array
-    arrayHistory[i] = 0;
-  }
   startIrPwm();
   wait();
 }
